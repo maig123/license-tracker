3810f06f5c85fd51cc1a928862704ad045491655
Alan <maig123@gmail.com>
2023-12-26 12:20:41 -0500

    update with landscape.csv

--- license-tracker.py
+++ license-tracker.py
@@ -11,25 +11,27 @@ import os
 user = os.getenv('GITHUBUSER')
 password = os.getenv('GITHUBPW')
 
-data = csv.reader(open("license-tracker/repos.csv", newline=''), delimiter=',')
+data = csv.reader(open("license-tracker/software.csv", newline=''), delimiter=',')
+with open('license-tracker/licenses.csv', mode="r") as infile:
+    reader = csv.reader(infile)
+    license_dict = {rows[0]:rows[11] for rows in reader}
+
+print(license_dict)
+
+
 g = Github(user,password)
 
-with open('license-tracker/license.csv', 'w', newline='') as file:
+with open('license-tracker/yourlicenses.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    for x in data: 
-        test = str(g.get_repo(x[1]).license.name)
-        if test == "Other":
-            url = str(g.get_repo(x[1]).get_license().html_url)
-            writer.writerow([x[0], x[1], test, url])
-        else:
-            writer.writerow([x[0], x[1], test])
-
-#container image name to github repo to lookup information. is there a better way of doing this? 
-container_dict = {
     
-    'quay.io/prometheus/prometheus': "prometheus/prometheus",
-    'grafana/grafana': "grafana/grafana",
-    'docker.elastic.co/elasticsearch/elasticsearch': "elasticsearch/elasticsearch",
-    'docker.elastic.co/kibana/kibana': "elasticsearch/kibana",
+    for x in data: 
+        if x[0] in license_dict:
+            writer.writerow([x[0], x[1], license_dict[x[0]]])
 
-}
+        else: 
+            checkrepo = str(g.get_repo(x[1]).license.name)
+            if checkrepo == "Other":
+                url = str(g.get_repo(x[1]).get_license().html_url)
+                writer.writerow([x[0], x[1], checkrepo, url])
+            else:
+                writer.writerow([x[0], x[1], checkrepo])
